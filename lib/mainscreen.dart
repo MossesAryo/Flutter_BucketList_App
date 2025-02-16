bdb8d70dfcca4335af530c3f82d856e34f823ff6
MossesAryo <mossesaryobimo@gmail.com>
2025-02-16 21:30:51 +0700

    UI Improvement If one of data is null

--- lib/mainscreen.dart
+++ lib/mainscreen.dart
@@ -66,23 +66,28 @@ class _MainScreenState extends State<MainScreen> {
         itemBuilder: (BuildContext context, int index) {
           return Padding(
             padding: const EdgeInsets.all(8.0),
-            child: ListTile(
-              onTap: () {
-                Navigator.push(context, MaterialPageRoute(builder: (context) {
-                  return ViewItemScreen(
-                    title: bucketListData[index]?["item"].toString() ?? "",
-                    image: bucketListData[index]?["image"] ?? "",
-                  );
-                }));
-              },
-              leading: CircleAvatar(
-                radius: 20,
-                backgroundImage:
-                    NetworkImage(bucketListData[index]?["image"] ?? ""),
-              ),
-              title: Text(bucketListData[index]?["item"] ?? ""),
-              trailing: Text(bucketListData[index]?["cost"].toString() ?? ""),
-            ),
+            child: (bucketListData[index] is Map)
+                ? ListTile(
+                    onTap: () {
+                      Navigator.push(context,
+                          MaterialPageRoute(builder: (context) {
+                        return ViewItemScreen(
+                          title:
+                              bucketListData[index]?["item"].toString() ?? "",
+                          image: bucketListData[index]?["image"] ?? "",
+                        );
+                      }));
+                    },
+                    leading: CircleAvatar(
+                      radius: 20,
+                      backgroundImage:
+                          NetworkImage(bucketListData[index]?["image"] ?? ""),
+                    ),
+                    title: Text(bucketListData[index]?["item"] ?? ""),
+                    trailing:
+                        Text(bucketListData[index]?["cost"].toString() ?? ""),
+                  )
+                : SizedBox(),
           );
         });
   }
