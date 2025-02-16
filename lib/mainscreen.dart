b52ea0723f81258debda563389c7894a931a95c1
MossesAryo <mossesaryobimo@gmail.com>
2025-02-16 16:00:14 +0700

    Setup DIO package in code

--- lib/mainscreen.dart
+++ lib/mainscreen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:dio/dio.dart';
 
 class MainScreen extends StatefulWidget {
   const MainScreen({super.key});
@@ -8,12 +9,19 @@ class MainScreen extends StatefulWidget {
 }
 
 class _MainScreenState extends State<MainScreen> {
+
+  void getData(){
+    // Get data From API
+
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Bucket List'),
       ),
+      body: ElevatedButton(onPressed: getData, child: Text('Get Data'))
     );
   }
 }
